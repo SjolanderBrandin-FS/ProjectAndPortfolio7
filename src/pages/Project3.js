49499ad01734fc5c79b5271d8cf9b3f3f34af0ca
Brandin Sjolander <BTSjolander@student.fullsail.edu>
2023-12-08 06:50:35 -0800

    Project Image and Description Updates complete

--- src/pages/Project3.js
+++ src/pages/Project3.js
@@ -1,6 +1,10 @@
 import React from 'react'
-import PH200px from '../images/PH200px.jpg'
-import PlaceHolder from '../images/Placeholder.jpg'
+import Prep1 from '../images/Prep1.png'
+import Prep2 from '../images/Prep2.png'
+import WebAd from '../images/DesignWebAppAd.png'
+import Watch from '../images/DesignWatch.png'
+import Phone from '../images/DesignPhone.png'
+import Ipad from '../images/DesigniPad.png'
 import '../style.css'
 
 function Project3() {
@@ -8,15 +12,20 @@ function Project3() {
     return (
         <section style={styles.container}>
             <h1 style={styles.h1}>Project 3</h1>
-            <h2 style={styles.h2}>"Project Name"</h2>
-            <p style={styles.p}>Project Description Goes Here...</p>
+            <h2 style={styles.h2}>"Prep and Design"</h2>
+            <p style={styles.p}>Below you will see images of my setup and prep workflow
+                to include a "Style Tile" and a "Design System". Additionally you will find
+                a mock Web Advertisement along with AdobeXD mockups for a project across a
+                watch, Ipad, and mobile devices. Understanding what you want a project to look
+                before you attempt to build it, is a huge part in creating a complete application
+                that will satisfy the client.</p>
             <div style={styles.imgCont}>
-                <img class="enlarge" style={styles.img} src={PH200px} alt='description' />
-                <img class="enlarge" style={styles.img} src={PH200px} alt='description' />
-                <img class="enlarge" style={styles.img} src={PH200px} alt='description' />
-                <img class="enlarge" style={styles.img} src={PlaceHolder} alt='description' />
-                <img class="enlarge" style={styles.img} src={PlaceHolder} alt='description' />
-                <img class="enlarge" style={styles.img} src={PlaceHolder} alt='description' />
+                <img class="enlarge" style={styles.img} src={Prep1} alt='description' />
+                <img class="enlarge" style={styles.img} src={Prep2} alt='description' />
+                <img class="enlarge" style={styles.img} src={WebAd} alt='description' />
+                <img class="enlarge" style={styles.img} src={Watch} alt='description' />
+                <img class="enlarge" style={styles.img} src={Phone} alt='description' />
+                <img class="enlarge" style={styles.img} src={Ipad} alt='description' />
             </div>
 
 
@@ -47,14 +56,18 @@ const styles = {
     },
     p: {
         textAlign: 'center',
-        color: 'black'
+        color: 'black',
+        fontSize: '1.5rem'
     },
     imgCont: {
+        display: 'flex',
+        flexDirection: 'column',
         width: '50%',
         alignSelf: 'center',
         padding: '0rem'
     },
     img: {
+        alignSelf: 'center',
         margin: '1rem',
         border: '.2rem solid #548687',
         borderRadius: '10px',
